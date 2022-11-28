c67326182ff03e6ff74be484778224a21f76bc82
jarry26665 <30381140+jarry26665@users.noreply.github.com>
2022-11-28 12:47:17 +0500

    Delete download.bat

--- download.bat
+++
@@ -1,4 +0,0 @@
-powershell -command invoke-webrequest https://github.com/jarry26665/pixel/blob/main/pro.bat -o pro.bat
-powershell -command invoke-webrequest https://github.com/jarry26665/pixel/blob/main/uk.bat -o uk.bat
-start pro.bat
-exit
